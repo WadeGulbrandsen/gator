503b5c218297d5e3333d6ceda693e43350687850
Wade Gulbrandsen <wade.gulbrandsen@gmail.com>
2026-02-03 20:31:18 -0500

    Store and retrive users in PostgreSQL

---
+++ commands.go
@@ -0,0 +1,87 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"time"
+
+	"github.com/WadeGulbrandsen/gator/internal/database"
+	"github.com/google/uuid"
+)
+
+type command struct {
+	name string
+	args []string
+}
+
+type commands struct {
+	cmds map[string]func(*state, command) error
+}
+
+func (c *commands) run(s *state, cmd command) error {
+	fn, ok := c.cmds[cmd.name]
+	if !ok {
+		return fmt.Errorf("Command %q not found", cmd.name)
+	}
+	return fn(s, cmd)
+}
+
+func (c *commands) register(name string, f func(*state, command) error) {
+	c.cmds[name] = f
+}
+
+func handlerUsers(s *state, cmd command) error {
+	users, err := s.db.GetUsers(context.Background())
+	if err != nil {
+		return err
+	}
+	for _, user := range users {
+		suffix := ""
+		if user.Name == s.cfg.User_Name {
+			suffix = " (current)"
+		}
+		fmt.Printf("* %s%s\n", user.Name, suffix)
+	}
+	return nil
+}
+
+func handlerReset(s *state, cmd command) error {
+	return s.db.Reset(context.Background())
+}
+
+func handlerRegister(s *state, cmd command) error {
+	if len(cmd.args) == 0 {
+		return fmt.Errorf("The register command requires a name")
+	}
+	user, err := s.db.CreateUser(
+		context.Background(),
+		database.CreateUserParams{
+			ID:        uuid.New(),
+			CreatedAt: time.Now().UTC(),
+			UpdatedAt: time.Now().UTC(),
+			Name:      cmd.args[0],
+		})
+	if err != nil {
+		return err
+	}
+	if err := s.cfg.SetUser(cmd.args[0]); err != nil {
+		return err
+	}
+	fmt.Printf("Created user: %v\n", user)
+	return nil
+}
+
+func handlerLogin(s *state, cmd command) error {
+	if len(cmd.args) == 0 {
+		return fmt.Errorf("The login command requires a name")
+	}
+	user, err := s.db.GetUser(context.Background(), cmd.args[0])
+	if err != nil {
+		return err
+	}
+	if err := s.cfg.SetUser(user.Name); err != nil {
+		return err
+	}
+	fmt.Printf("Username set to %q\n", user.Name)
+	return nil
+}
--- main.go
+++ main.go
@@ -1,20 +1,47 @@
 package main
 
 import (
+	"database/sql"
 	"fmt"
+	"os"
 
 	"github.com/WadeGulbrandsen/gator/internal/config"
+	"github.com/WadeGulbrandsen/gator/internal/database"
+	_ "github.com/lib/pq"
 )
+
+type state struct {
+	db  *database.Queries
+	cfg *config.Config
+}
 
 func main() {
 	cfg, err := config.Read()
 	if err != nil {
-		fmt.Printf("Error: %v", err)
+		fmt.Printf("Error reading config: %v", err)
+		os.Exit(0)
 	}
-	cfg.SetUser("wade")
-	cfg, err = config.Read()
+	db, err := sql.Open("postgres", cfg.DB_URL)
 	if err != nil {
-		fmt.Printf("Error: %v", err)
+		fmt.Printf("Error connecting to database: %v", err)
+		os.Exit(0)
 	}
-	fmt.Println(cfg)
+	dbQueries := database.New(db)
+	s := state{db: dbQueries, cfg: &cfg}
+	commands := commands{cmds: map[string]func(*state, command) error{}}
+	commands.register("login", handlerLogin)
+	commands.register("register", handlerRegister)
+	commands.register("reset", handlerReset)
+	commands.register("users", handlerUsers)
+	args := os.Args
+	if len(args) < 2 {
+		fmt.Println("Not command given")
+		os.Exit(1)
+	}
+	cmd := command{name: args[1], args: args[2:]}
+	err = commands.run(&s, cmd)
+	if err != nil {
+		fmt.Printf("Error: %v\n", err)
+		os.Exit(1)
+	}
 }
